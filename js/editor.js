d90e5a872e79048b420f8710288509f042bee088
GodXero <godxero.dev@gmail.com>
2025-05-13 16:40:17 +0530

    drop png image catch and store in a array

--- js/editor.js
+++ js/editor.js
@@ -5,6 +5,8 @@ class Editor {
 		this.height = 0;
 
 		this.objects = [];
+		this.loadedImagesPaths = new Set();
+		this.loadedImages = [];
 
 		this.#initEvents();
 	}
@@ -15,6 +17,46 @@ class Editor {
 		window.addEventListener('mouseup', this.#mouseup.bind(this));
 		window.addEventListener('keydown', this.#keydown.bind(this));
 		window.addEventListener('keyup', this.#keyup.bind(this));
+
+		this.canvas.addEventListener('dragover', (event) => {
+			event.preventDefault();
+		});
+
+		this.canvas.addEventListener('drop', (event) => {
+			event.preventDefault();
+
+			const files = event.dataTransfer.files;
+			
+			if (files.length == 0 || files[0].type !== 'image/png') {
+				console.warn('Wrong file');
+				return;
+			}
+
+			const file = files[0];
+			const reader = new FileReader();
+
+			reader.addEventListener('load', (event) => {
+				const src =  event.target.result;
+
+				if (this.loadedImagesPaths.has(src)) {
+					// Image is already loaded
+					return;
+				}
+
+				const img = new Image();
+
+				img.addEventListener('load', () => {
+					// Image loaded
+				});
+
+				this.loadedImagesPaths.add(src);
+				this.loadedImages.push(img);
+
+				img.src = src;
+			});
+
+			reader.readAsDataURL(file);
+		});
 	}
 
 	#mousedown (event) {}
