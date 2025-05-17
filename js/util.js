cfa15fe028b9c03b16d1f79db02c3ef85bd820a5
GodXero <godxero.dev@gmail.com>
2025-05-17 22:23:30 +0530

    removed dots from select rectangles from objects while images will not streth future

--- js/util.js
+++ js/util.js
@@ -4,7 +4,7 @@ function drawDashedRect (ctx, rect, color = '#ffffff', scale = 1) {
 	ctx.save();
 
 	ctx.strokeStyle = color;
-	ctx.lineWidth = 2 / scale;
+	ctx.lineWidth = 5 / scale;
 
 	ctx.setLineDash([5 / scale, 5 / scale]);
 	ctx.strokeRect(rect.x, rect.y, rect.w, rect.h);
@@ -16,42 +16,11 @@ function drawSelectRect (ctx, rect, color = '#ffffff', scale = 1) {
 	ctx.save();
 
 	ctx.strokeStyle = color;
-	ctx.lineWidth = 2 / scale;
+	ctx.lineWidth = 5 / scale;
 	ctx.shadowColor = '#000000';
 	ctx.shadowBlur = 5;
 
 	ctx.strokeRect(rect.x, rect.y, rect.w, rect.h);
-
-	ctx.lineWidth = EDITOR_SETTINGS.selector_dots_size / scale;
-	ctx.lineCap = 'round';
-
-	ctx.beginPath();
-
-	ctx.moveTo(rect.x, rect.y);
-	ctx.lineTo(rect.x, rect.y);
-
-	ctx.moveTo(rect.x + rect.w * 0.5, rect.y);
-	ctx.lineTo(rect.x + rect.w * 0.5, rect.y);
-
-	ctx.moveTo(rect.x + rect.w, rect.y);
-	ctx.lineTo(rect.x + rect.w, rect.y);
-
-	ctx.moveTo(rect.x + rect.w, rect.y + rect.h * 0.5);
-	ctx.lineTo(rect.x + rect.w, rect.y + rect.h * 0.5);
-rect
-	ctx.moveTo(rect.x + rect.w, rect.y + rect.h);
-	ctx.lineTo(rect.x + rect.w, rect.y + rect.h);
-
-	ctx.moveTo(rect.x + rect.w * 0.5, rect.y + rect.h);
-	ctx.lineTo(rect.x + rect.w * 0.5, rect.y + rect.h);
-
-	ctx.moveTo(rect.x, rect.y + rect.h);
-	ctx.lineTo(rect.x, rect.y + rect.h);
-
-	ctx.moveTo(rect.x, rect.y + rect.h * 0.5);
-	ctx.lineTo(rect.x, rect.y + rect.h * 0.5);
-
-	ctx.stroke();
 }
 
 function isPointInRect (rect, x, y, offset = 0) {
